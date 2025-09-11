0c3349fb4b96d4e215e7d1ea3f40e418bcd4d0d1
Valery Rabchenyuk <valery0623@gmail.com>
2025-09-11 19:35:57 +0300

    payment model

--- backend/payment/models.py
+++ backend/payment/models.py
@@ -1,3 +1,55 @@
-from django.db import models
+from django.db import models, transaction
+from django.db.models import F
 
-# Create your models here.
+from collect.models import Collect
+from django.contrib.auth.models import User
+
+
+class Payment(models.Model):
+    """Платёж для сбора."""
+    collect = models.ForeignKey(
+        Collect,
+        on_delete=models.CASCADE,
+        related_name='payments',
+        verbose_name='Сбор'
+    )
+    donor = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        verbose_name='Донор'
+    )
+    amount = models.DecimalField(
+        max_digits=12,
+        decimal_places=2,
+        verbose_name='Сумма пожертвования'
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата и время пожертвования'
+    )
+
+    def __str__(self):
+        return f'{self.amount} -> {self.collect.title}'
+
+    def save(self, *args, **kwargs):
+        is_new = self.pk is None
+
+        if is_new:
+            with transaction.atomic():
+                collect = Collect.objects.select_for_update().\
+                    get(id=self.collect.id)
+
+                is_first_payment = not Payment.objects.\
+                    filter(collect=collect, donor=self.donor).exists()
+
+                update_data = {
+                    'collected_amount': F('collected_amount') + self.amount
+                }
+
+                if is_first_payment:
+                    update_data['donors_count'] = F('donors_count') + 1
+
+                Collect.objects.filter(id=collect.id).update(**update_data)
+
+        super().save(*args, **kwargs)
