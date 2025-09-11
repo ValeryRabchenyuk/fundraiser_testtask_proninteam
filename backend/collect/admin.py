7f709b4487edeb64bb8363af6e3e3e022c3b0655
Valery Rabchenyuk <valery0623@gmail.com>
2025-09-11 19:34:53 +0300

    collect model

--- backend/collect/admin.py
+++ backend/collect/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from .models import Collect
+
+@admin.register(Collect)
+class CollectAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'author',
+        'title',
+        'reason',
+        'target_amount',
+        'collected_amount',
+        'donors_count',
+        'created_at',
+        'ends_at'
+    )
+    list_filter = ('author', 'reason', 'created_at', 'ends_at')
+    search_fields = ('author__username', 'title', 'created_at')
+    list_editable = ('title', 'reason', 'target_amount', 'ends_at')
