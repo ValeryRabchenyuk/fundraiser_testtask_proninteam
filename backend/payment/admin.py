9ba4fab9613ba416cb4731e7688ef55914e32e98
Valery Rabchenyuk <valery0623@gmail.com>
2025-09-13 13:22:53 +0300

    PaymentAdmin

--- backend/payment/admin.py
+++ backend/payment/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from .models import Payment
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'collect',
+        'donor',
+        'amount',
+        'created_at',
+    )
+    list_filter = ('created_at',)
+    search_fields = ('author__username', 'collect', 'created_at')
